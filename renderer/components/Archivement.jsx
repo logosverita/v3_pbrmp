5be6df26924d37b5caf2b55c417ecb80b9c3c10d
ゆ <kip.source.wizard@gmail.com>
2021-10-06 12:22:06 +0900

    Cal Heatmap 外側だけ完成

--- renderer/components/Archivement.jsx
+++ renderer/components/Archivement.jsx
@@ -1,104 +1,29 @@
 // React
 import { react, useState, useEffect } from 'react';
 // ライブラリ
-import ApexCharts from 'apexcharts';
+import CalendarHeatmap from '@hackclub/react-calendar-heatmap'
 //スタイル
 import AM from '../style/archvement.module.css';
+import '@hackclub/react-calendar-heatmap/dist/styles.css'
 // コンポーネント
 
 // マテリアルUI
 //マテリアルUI Icon
 const Archivement = () => {
-    class ApexChart extends React.Component {
-        constructor(props) {
-            super(props);
-
-            this.state = {
-            
-                series: [{
-                    name: 'Metric1',
-                    data: generateData(18, {
-                    min: 0,
-                    max: 90
-                    })
-                },
-                {
-                    name: 'Metric2',
-                    data: generateData(18, {
-                    min: 0,
-                    max: 90
-                    })
-                },
-                {
-                    name: 'Metric3',
-                    data: generateData(18, {
-                    min: 0,
-                    max: 90
-                    })
-                },
-                {
-                    name: 'Metric4',
-                    data: generateData(18, {
-                    min: 0,
-                    max: 90
-                    })
-                },
-                {
-                    name: 'Metric5',
-                    data: generateData(18, {
-                    min: 0,
-                    max: 90
-                    })
-                },
-                {
-                    name: 'Metric6',
-                    data: generateData(18, {
-                    min: 0,
-                    max: 90
-                    })
-                },
-                {
-                    name: 'Metric7',
-                    data: generateData(18, {
-                    min: 0,
-                    max: 90
-                    })
-                },
-                {
-                    name: 'Metric8',
-                    data: generateData(18, {
-                    min: 0,
-                    max: 90
-                    })
-                },
-                {
-                    name: 'Metric9',
-                    data: generateData(18, {
-                    min: 0,
-                    max: 90
-                    })
-                }
-                ],
-                options: {
-                    chart: {
-                    height: 350,
-                    type: 'heatmap',
-                    },
-                    dataLabels: {
-                    enabled: false
-                    },
-                    colors: ["#008FFB"],
-                    title: {
-                    text: 'HeatMap Chart (Single color)'
-                    },
-                },
-            }
-        }
-    }
     return (
         <>
-        <div id="chart">
-            <ReactApexChart options={this.state.options} series={this.state.series} type="heatmap" height={350} />
+        <div className={AM.heatmap}>
+            <CalendarHeatmap
+                startDate={new Date('2021-01-01')}
+                endDate={new Date('2021-12-31')}
+                showWeekdayLabels={true}
+                values={[
+                    { date: '2021-01-01', count: 12 },
+                    { date: '2021-01-22', count: 122 },
+                    { date: '2021-01-30', count: 38 },
+                    // ...and so on
+                ]}
+            />
         </div>
         </>
     )
